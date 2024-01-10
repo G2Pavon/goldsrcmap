70da0db65159f5cf72147952f01cc25bdbb26430
Gaspar <gasparguidopavon@gmail.com>
2024-01-10 12:59:11 -0300

    Simplified docstrings and ordered methods

--- src/format/map/face.py
+++ src/format/map/face.py
@@ -10,10 +10,17 @@ from utils.math.point import Point
 
 class Face:
     """
-    ( x1 y1 z1 ) ( x2 y2 z2 ) ( x3 y3 z3 ) TEXTURE [ Ux Uy Uz Uoffset ] [ Vx Vy Vz Voffset ] rotation Uscale Vscale
-
+    Plane: ( x1 y1 z1 ) ( x2 y2 z2 ) ( x3 y3 z3 ) 
+    Texture definition: NAME [ Ux Uy Uz Uoffset ] [ Vx Vy Vz Voffset ] rotation Uscale Vscale
+                
     Three non-coplanar points define a plane
     Plane normal is oriented toward the cross product of (P1-P2) and (P3-P2)
+
+    Attributes:
+    - id (int): The unique identifier for the face.
+    - plane (Plane): The Plane that compose the face.
+    - texture (Texture): The texture definition
+    - vertices (list[Point]): List of vertices
     """
 
     def __init__(self, plane: Plane, texture: Texture):
@@ -29,17 +36,7 @@ class Face:
         return f"{self.plane} {self.texture} \n"
 
     def __iter__(self):
-        """Returns an iterator over the plane of the face
-
-        Example:
-        >>> # Traditional syntax
-        >>> for point in self.plane:
-                # Your logic...
-        >>>
-        >>> # New alternative syntax
-        >>> for point in self:
-                # Your logic...
-        """
+        """Return an iterator over the face plane points"""
         return iter((self.plane))
 
     def __contains__(self, other: Union[str, Point]) -> bool:
@@ -58,14 +55,20 @@ class Face:
     def texture_normal(self) -> Vector3:
         """Returns the normal vector of the face's texture"""
         return self.texture.u_axis.cross(self.texture.v_axis).normalize()
+    
+    def centroid(self) -> Union[Point, None]:
+        """Calculates the centroid of the face"""
+        if not self.plane.collinear_points():
+            centroid_x = sum(point.x for point in self.plane) / 3
+            centroid_y = sum(point.y for point in self.plane) / 3
+            centroid_z = sum(point.z for point in self.plane) / 3
 
-    def _add_vertex(self, vertex: Point):
-        """Add a vertex to the face"""
-        self.vertices.append(vertex)
-
-    def set_texture(self, text_name: str):
-        """Sets the texture name of the face"""
-        self.texture.name = text_name
+            return Point(centroid_x, centroid_y, centroid_z)
+        return None
+    
+    def copy(self) -> 'Face':
+        """Creates a deep copy of the face"""
+        return deepcopy(self)
 
     def has_texture(self, text_name: str, exact: bool = True):
         """Checks if the face has a specific texture"""
@@ -83,7 +86,7 @@ class Face:
         return True
 
     def move_by(self, x: float, y: float, z: float):
-        """Moves the face by specified offsets"""
+        """Moves the face by a specified offsets"""
         for point in self.plane:
             point.move_by(x, y, z)
         self.texture._update_offset(Vector3(x, y, z))
@@ -103,18 +106,14 @@ class Face:
         self.texture._update_offset(Vector3(0, 0, 0))
 
     def rotate_z(self, angle: float, center: Point):
-        """
-        Rotates the face around the Z-axis
-        """
+        """Rotates the face around the Z-axis"""
         for point in self.plane:
             point.rotate_z(angle, center)
         self.texture._rotate_uv_x(angle)
         self.texture._update_offset(Vector3(0, 0, 0))
 
     def rotate_xyz(self, phi, theta, psi):
-        """
-        Rotates the face around the XYZ axes
-        """
+        """Rotates the face around the XYZ axes"""
         for point in self.plane:
             point.rotate_xyz(phi, theta, psi)
         self.texture._rotate_uv_x(phi)
@@ -123,27 +122,15 @@ class Face:
         self.texture._update_offset(Vector3(0,0,0))
 
     def rotate_around_axis(self, angle: float, axis: Vector3):
-        """
-        Rotate the face around the given axis
-        """
+        """Rotate the face around the given axis"""
         for point in self.plane:
             point.rotate_around_axis(angle, axis)
         self.texture._rotate_uv_around_axis(angle, axis)
 
-    def copy(self) -> 'Face':
-        """
-        Creates a deep copy of the face
-        """
-        return deepcopy(self)
-    
-    def centroid(self) -> Union[Point, None]:
-        """
-        Calculates the centroid of the face
-        """
-        if not self.plane.collinear_points():
-            centroid_x = sum(point.x for point in self.plane) / 3
-            centroid_y = sum(point.y for point in self.plane) / 3
-            centroid_z = sum(point.z for point in self.plane) / 3
+    def set_texture(self, text_name: str):
+        """Sets the texture name of the face"""
+        self.texture.name = text_name
 
-            return Point(centroid_x, centroid_y, centroid_z)
-        return None
+    def _add_vertex(self, vertex: Point):
+        """Add a vertex to the face"""
+        self.vertices.append(vertex)
