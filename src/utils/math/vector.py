e343ae97d8ab1acc0f1803752c1c40aca3c78ce3
Gaspar <gasparguidopavon@gmail.com>
2024-01-18 21:15:59 -0300

    update vector class

--- src/utils/math/vector.py
+++ src/utils/math/vector.py
@@ -45,25 +45,23 @@ class Vector3:
         """Returns a normalized version of the vector"""
         length = self.length()
         if length == 0:
-            return 0
+            raise ValueError("Cannot normalize a zero-length vector.")
         return Vector3(self.x / length, self.y / length, self.z / length)
+
 
     def dot(self, other: 'Vector3') -> float:
         """Computes the dot product of this vector with another vector"""
-        if isinstance(other, Vector3):
-            result = self.x * other.x + self.y * other.y + self.z * other.z
-        else:
+        if not isinstance(other, Vector3):
             raise TypeError(f"Unsupported type for dot product: {type(other)}")
-        return result
+        return self.x * other.x + self.y * other.y + self.z * other.z
 
     def cross(self, other: 'Vector3') -> 'Vector3':
         """Computes the cross product of this vector with another vector"""
-        if isinstance(other, Vector3):
-            x = self.y * other.z - self.z * other.y
-            y = self.z * other.x - self.x * other.z
-            z = self.x * other.y - self.y * other.x
-        else:
+        if not isinstance(other, Vector3):
             raise TypeError(f"Unsupported type for cross product: {type(other)}")
+        x = self.y * other.z - self.z * other.y
+        y = self.z * other.x - self.x * other.z
+        z = self.x * other.y - self.y * other.x
         return Vector3(x, y, z)
 
     def is_null(self, threshold: float = 1e-4) -> bool:
@@ -72,19 +70,17 @@ class Vector3:
 
     def is_parallel(self, other: 'Vector3', threshold: float = 1e-4) -> bool:
         """Checks if the vector is parallel to another vector"""
-        if isinstance(other, Vector3):
-            dot_product = self.dot(other)
-            magnitude_product = self.length() * other.length()
-            return abs(dot_product - magnitude_product) < threshold
-        else:
+        if not isinstance(other, Vector3):
             raise TypeError(f"Unsupported type for parallel check: {type(other)}")
+        dot_product = self.dot(other)
+        magnitude_product = self.length() * other.length()
+        return abs(dot_product - magnitude_product) < threshold
 
     def is_perpendicular(self, other: 'Vector3', threshold: float = 1e-4) -> bool:
         """Checks if the vector is perpendicular to another vector"""
-        if isinstance(other, Vector3):
-            return abs(self.dot(other)) < threshold
-        else:
+        if not isinstance(other, Vector3):
             raise TypeError(f"Unsupported type for perpendicular check: {type(other)}")
+        return abs(self.dot(other)) < threshold
     
 
 # ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
@@ -109,74 +105,61 @@ class Vector3:
 
     def __add__(self, other: 'Vector3') -> 'Vector3':
         """Adds another vector to this vector"""
-        if isinstance(other, Vector3):
-            return Vector3(self.x + other.x, self.y + other.y, self.z + other.z)
-        else:
+        if not isinstance(other, Vector3):
             raise TypeError(f"Unsupported type for addition (+): {type(other)}")
+        return Vector3(self.x + other.x, self.y + other.y, self.z + other.z)
 
     def __sub__(self, other: 'Vector3') -> 'Vector3':
         """Subtracts another vector from this vector"""
-        if isinstance(other, Vector3):
-            return Vector3(self.x - other.x, self.y - other.y, self.z - other.z)
-        else:
+        if not isinstance(other, Vector3):
             raise TypeError(f"Unsupported type for subtraction (-): {type(other)}")
+        return Vector3(self.x - other.x, self.y - other.y, self.z - other.z)
 
     def __mul__(self, other: float) -> 'Vector3':
         """Multiplies the vector by a scalar"""
-        if isinstance(other, (int, float)):
-            return Vector3(self.x * other, self.y * other, self.z * other)
-        else:
+        if not isinstance(other, float):
             raise TypeError(f"Unsupported type for multiplication (*): {type(other)}")
+        return Vector3(self.x * other, self.y * other, self.z * other)
         
     def __imul__(self, other: float) -> 'Vector3':
         """In-place multiplication with a scalar"""
-        if isinstance(other, (int, float)):
-            self.x *= other
-            self.y *= other
-            self.z *= other
-        else:
+        if not isinstance(other, float):
             raise TypeError(f"Unsupported type for in-place division (*=): {type(other)}")
+        self.x *= other
+        self.y *= other
+        self.z *= other
         return self
 
     def __rmul__(self, other: float) -> 'Vector3':
         """Right multiplication with a scalar"""
-        if isinstance(other, (int, float)):
-            return Vector3(other * self.x, other * self.y, other * self.z)
-        else:
+        if not isinstance(other, float):
             raise TypeError(f"Unsupported type for right multiplication (*): {type(other)}")
-
+        return Vector3(other * self.x, other * self.y, other * self.z)
+    
     def __truediv__(self, other: float) -> 'Vector3':
         """Divides the vector by a scalar"""
-        if isinstance(other, (int, float)):
-            return Vector3(self.x / other, self.y / other, self.z / other)
-        else:
+        if not isinstance(other, float):
             raise TypeError(f"Unsupported type for division (/): {type(other)}")
+        return Vector3(self.x / other, self.y / other, self.z / other)
 
     def __itruediv__(self, other: float) -> 'Vector3':
         """In-place division with a scalar"""
-        if other == 0:
-            raise ZeroDivisionError
-        if isinstance(other, (int, float)):
-            self.x /= other
-            self.y /= other
-            self.z /= other
-        else:
+        if not isinstance(other, float):
             raise TypeError(f"Unsupported type for in-place division (/=): {type(other)}")
+        if other == 0:
+            raise ZeroDivisionError("Division by zero")
+        self.x /= other
+        self.y /= other
+        self.z /= other
         return self
 
     def __eq__(self, other: 'Vector3') -> bool:
         """Checks if two vectors are equal"""
-        if isinstance(other, Vector3):
-            return (self.x, self.y, self.z) == (other.x, other.y, other.z)
-        else:
-            raise TypeError(f"Unsupported type for equality comparison: {type(other)}")
+        return isinstance(other, Vector3) and (self.x, self.y, self.z) == (other.x, other.y, other.z)
 
     def __ne__(self, other: 'Vector3') -> bool:
         """Checks if two vectors are not equal"""
-        if isinstance(other, Vector3):
-            return not self.__eq__(other)
-        else:
-            raise TypeError(f"Unsupported type for inequality comparison: {type(other)}")
+        return not self.__eq__(other)
 
     def __neg__(self) -> 'Vector3':
         """Negates the vector"""
