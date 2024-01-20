f48114f7a9961ce0204b08d6a73ebce1b2cf6c65
Gaspar <gasparguidopavon@gmail.com>
2024-01-20 16:17:35 -0300

    simplified entitygenerator snippet

--- src/utils/entitygenerator.py
+++ src/utils/entitygenerator.py
@@ -3,23 +3,15 @@ from format.map.entity import Entity
 class EntityGenerator:
 
     @staticmethod
-    def info_player_start(origin: list, angle: int = 0) -> Entity:
-        e = Entity()
-        e.properties = {
-                        "classname":"info_player_start",
-                        "origin":f"{origin[0]} {origin[1]} {origin[2]}",
-                        "angles":f"0 {angle} 0",
-                        }
-        return e
+    def info_player_start(origin: list, yaw: int = 0) -> Entity:
+        return Entity('info_player_start',origin, {"angles":f"0 {yaw} 0"})
     
     @staticmethod
-    def light_environment(origin: list, angle: int = 0, pitch: int = -90) -> Entity:
-        e = Entity()
-        e.properties = {
-                        "classname":"light_environment",
-                        "angle":f"{angle}",
+    def light_environment(origin: list, yaw: int = 0, pitch: int = -90, color: list[int] = [255, 255, 255, 200] ) -> Entity:
+        return Entity('light_environment', origin, 
+                    {
+                        "angles":f"0 {yaw} 0",
                         "pitch":f"{pitch}",
-                        "_light":"255 255 255 200",
-                        "origin":f"{origin[0]} {origin[1]} {origin[2]}"
-                        }
-        return e
+                        "_light":f"{color[0]} {color[1]} {color[2]} {color[3]}"
+                    }
+        )
