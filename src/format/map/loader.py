65fb0984c01924649760d2e7690357015274657f
Gaspar <gasparguidopavon@gmail.com>
2024-02-28 21:29:20 -0300

    load_map() is a little faster

--- src/format/map/loader.py
+++ src/format/map/loader.py
@@ -9,78 +9,70 @@ from utils.math.point import Point
 from utils.math.vector import Vector3
 
 import os
-# Lazy implementation
-#TODO: Class Reader
 
-def extract_pathname(filepath: str):
-    path, filename = os.path.split(filepath)
-    mapname, extension = os.path.splitext(filename)
-    if extension == '.map':
-        return path, mapname
-
-
-def load_map(filepath:str) -> Map:
-    map_path, name = extract_pathname(filepath)
+def load_map(filepath: str) -> Map:
+    map_path, name = os.path.split(filepath)
     map_obj = Map(name)
     map_obj.path = map_path
+    map_obj.entity_counter = -1
 
-    brace_count = 0
-    current_entity = None
-    current_brush = None
-    
     with open(filepath, 'r') as file:
-        for line in file.readlines():
+        entities = []
+        current_entity = None
+        current_brush = None
+        brace_count = 0
 
-            #skip comments
-            if line.startswith('//'):
+        for line in file:
+            tokens = line.strip().split()
+            if not tokens or line.startswith('//'):
                 continue
 
-            elif line.startswith('{'):
+            if tokens[0] == '{':
                 brace_count += 1
                 if brace_count == 1:
-                    #start of the entity
                     current_entity = Entity()
                 else:
-                    #start of the brush
                     current_brush = Brush()
-                    
-            elif line.startswith('}'):
+
+            elif tokens[0] == '}':
                 brace_count -= 1
                 if brace_count == 0:
-                    #end of the entity
-                    map_obj.add_entity(current_entity)
+                    map_obj.entity_counter += 1
+                    current_entity._id = map_obj.entity_counter
+                    current_entity.in_map_instance = True
+                    entities.append(current_entity)
+                else:
+                    current_brush._id = current_entity.brush_counter
+                    current_entity.brushes.append(current_brush)
+                    current_entity.brush_counter += 1
 
-                else: # brace_count == 1
-                    #end of the brush
-                    current_entity.add_brush(current_brush)
-
-            #entity property, brace_count == 1
-            elif line.startswith('"'):
-                k, v = line.strip().split('" "', 1)
-                key, value = k.replace('"', ''), v.replace('"', '')
-                current_entity.properties[key] = value
-            
-            #brush face, brace_count == 2
-            elif line.startswith('('):
-                face_info = line.strip().split()
-                if len(face_info) <= 31:
+            elif tokens[0] == '(':
+                if len(tokens) <= 31:
                     plane = Plane(
-                        Point(float(face_info[1]), float(face_info[2]), float(face_info[3])),
-                        Point(float(face_info[6]), float(face_info[7]), float(face_info[8])),
-                        Point(float(face_info[11]), float(face_info[12]), float(face_info[13]))
+                        Point(float(tokens[1]), float(tokens[2]), float(tokens[3])),
+                        Point(float(tokens[6]), float(tokens[7]), float(tokens[8])),
+                        Point(float(tokens[11]), float(tokens[12]), float(tokens[13]))
                     )
-                    texture_name = str(face_info[15]).upper()
-                    u_axis =  Vector3(float(face_info[17]), float(face_info[18]), float(face_info[19]))
-                    u_offset = float(face_info[20])
-                    v_axis = Vector3(float(face_info[23]), float(face_info[24]), float(face_info[25]))
-                    v_offset = float(face_info[26])
-                    rotation = float(face_info[28])
-                    u_scale = float(face_info[29])
-                    v_scale = float(face_info[30])
+                    texture_name = tokens[15].upper()
+                    u_axis = Vector3(float(tokens[17]), float(tokens[18]), float(tokens[19]))
+                    u_offset = float(tokens[20])
+                    v_axis = Vector3(float(tokens[23]), float(tokens[24]), float(tokens[25]))
+                    v_offset = float(tokens[26])
+                    rotation = float(tokens[28])
+                    u_scale = float(tokens[29])
+                    v_scale = float(tokens[30])
 
                     texture = Texture(texture_name, u_axis, u_offset, v_axis, v_offset, rotation, u_scale, v_scale)
                     current_face = Face(plane, texture)
 
-                    current_brush.add_face(current_face)
-    return map_obj
+                    current_brush.face_counter += 1
+                    current_face.id = current_brush.face_counter
+                    current_brush.faces.append(current_face)
 
+            else:
+                k, v = line.strip().split('" "', 1)
+                key, value = k.replace('"', ''), v.replace('"', '')
+                current_entity.properties[key] = value
+
+    map_obj.entities = entities
+    return map_obj
